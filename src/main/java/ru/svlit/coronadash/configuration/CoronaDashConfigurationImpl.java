5adbd153dd9014ba8c7257c28be76ea4570271f0
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-11-07 20:36:55 +0300

    Изменил способ подключения к MongoDB

--- src/main/java/ru/svlit/coronadash/configuration/CoronaDashConfigurationImpl.java
+++ src/main/java/ru/svlit/coronadash/configuration/CoronaDashConfigurationImpl.java
@@ -2,14 +2,17 @@ package ru.svlit.coronadash.configuration;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static java.lang.String.format;
-import static ru.svlit.coronadash.configuration.CoronaDashConfigurationConstants.CONFIGURATION_PREFIX;
+import static java.lang.System.getenv;
+import static ru.svlit.coronadash.configuration.CoronaDashConfigurationConstants.*;
 
 @Data
+@Slf4j
 @Configuration
 @EnableScheduling
 @NoArgsConstructor
@@ -17,15 +20,25 @@ import static ru.svlit.coronadash.configuration.CoronaDashConfigurationConstants
 class CoronaDashConfigurationImpl implements CoronaDashConfiguration {
     private String sourceUrl;
     private Long refreshDelayMs;
-    private String databaseProtocol;
-    private String databaseHost;
-    private String databaseUser;
-    private String databasePassword;
-    private String databaseName;
 
     @Override
     public String getDataBaseConnectionString() {
+        String databaseProtocol = getenv(ENV_CD_DATABASE_PROTOCOL);
+        String databaseHost = getenv(ENV_CD_DATABASE_HOST);
+        String databaseUser = getenv(ENV_CD_DATABASE_USER);
+        String databasePassword = getenv(ENV_CD_DATABASE_PASSWORD);
+        String databaseName = getenv(ENV_CD_DATABASE_NAME);
         final String template = "%s://%s:%s@%s/%s";
-        return format(template, databaseProtocol, databaseUser, databasePassword, databaseHost, databaseName);
+        final String connectionString = format(
+                template,
+                databaseProtocol,
+                databaseUser,
+                databasePassword,
+                databaseHost,
+                databaseName
+        );
+
+        log.info("Адрес БД вычислен: " + connectionString);
+        return connectionString;
     }
 }
