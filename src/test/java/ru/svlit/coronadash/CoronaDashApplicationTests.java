cf8b2f2a55a59624d2b971108ba2ba44316564a0
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-11-07 18:42:27 +0300

    Initial commit

---
+++ src/test/java/ru/svlit/coronadash/CoronaDashApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svlit.coronadash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
