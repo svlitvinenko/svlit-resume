a279a1bf20e7c22d0ca8999655274532a0c24c3a
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-11-19 17:12:22 +0300

    Страница с предложением првоерить почту для активации

--- src/main/java/ru/svlit/feature/authentication/adapter/web/ActivationController.java
+++ src/main/java/ru/svlit/feature/authentication/adapter/web/ActivationController.java
@@ -3,13 +3,18 @@ package ru.svlit.feature.authentication.adapter.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.svlit.architecture.annotation.WebAdapter;
+import ru.svlit.core.util.NavigationContent;
 import ru.svlit.core.util.Redirect;
+import ru.svlit.core.util.UnifiedModelAndView;
 import ru.svlit.feature.authentication.application.port.in.ConfirmSignUpUseCase;
 import ru.svlit.feature.authentication.application.port.in.ConfirmSignUpUseCase.ActivationCodeNotFoundException;
 import ru.svlit.feature.authentication.application.port.in.ConfirmSignUpUseCase.ConfirmSignUpCommand;
+import ru.svlit.feature.home.application.port.in.GetNavigationContentUseCase;
+import ru.svlit.feature.home.application.port.in.GetNavigationContentUseCase.GetNavigationContentCommand;
 
 import static ru.svlit.feature.home.configuration.HomeConfigurationConstants.PATH_ACTIVATION;
 import static ru.svlit.feature.home.configuration.HomeConfigurationConstants.PATH_SIGN_IN;
@@ -27,6 +32,19 @@ class ActivationController {
     private static final String PATH_ACTIVATE_BY_CODE = "/{code}";
 
     private final ConfirmSignUpUseCase confirmSignUpUseCase;
+    private final GetNavigationContentUseCase getNavigationContentUseCase;
+
+    @GetMapping
+    public String getNeedsActivationPage(@ModelAttribute("username") String username, @ModelAttribute("email") String email, Model model) {
+        final NavigationContent navigationContent = getNavigationContentUseCase.perform(
+                new GetNavigationContentCommand(false)
+        );
+        final UnifiedModelAndView modelAndView = new UnifiedModelAndView("activation", navigationContent);
+        modelAndView.addObject("username", username);
+        modelAndView.addObject("email", email);
+        model.addAllAttributes(modelAndView.getModel());
+        return "activation";
+    }
 
     @GetMapping(PATH_ACTIVATE_BY_CODE)
     public String activate(@PathVariable String code) {
